a2c82709a2cd714c0e72122337dd7460b81251f7
yuenee <yuenee.op@gmail.com>
2022-03-31 23:48:55 +0900

    helloコミット

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { HelloComponent } from './hello/hello.component';
     HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [HelloComponent]
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -6,15 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hello.component.scss']
 })
 export class HelloComponent implements OnInit {
-  title: string;
-  message: string;
+  title: string = '';
+  message: string = '';
+  count: number = 0;
+  text: string = '';
 
-  constructor() {
-    this.title = 'Hello-app';
-    this.message = 'This is My First Component!!';
-  }
+  constructor() {}
 
   ngOnInit(): void {
+    this.title = 'Hello-app';
+    this.message = '※ボタンをクリック！';
+    this.count = 0;
   }
 
+  doClick() {
+    this.message = ++this.count + "回、クリックしました。";
+  }
 }
